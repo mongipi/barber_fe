bbfc9660454436cda56e56d42cde99c058346d23
PietroMongiello <pietro.mongiello@beije.it>
2023-11-28 14:26:33 +0100

    clienti non funziona

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -3,19 +3,24 @@ const navigation = () => {
     {
       title: 'Home',
       path: '/home',
-      icon: 'tabler:smart-home',
+      icon: 'tabler:smart-home'
     },
     {
       title: 'Second Page',
       path: '/second-page',
-      icon: 'tabler:mail',
+      icon: 'tabler:mail'
+    },
+    {
+      title: 'Clienti',
+      path: '/clienti/list',
+      icon: 'tabler:user'
     },
     {
       path: '/acl',
       action: 'read',
       subject: 'acl-page',
       title: 'Access Control',
-      icon: 'tabler:shield',
+      icon: 'tabler:shield'
     }
   ]
 }
